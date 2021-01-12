357d4728eec39bac7bb6997d0f4cd5d4ff65cf3f
Usama Sadiq <usamasadiq029@gmail.com>
2021-01-12 11:28:18 +0500

    Complete static total confirmed, recovered, deaths counting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
+import Card from "./components/Card";
 import LineGraph from "./components/LineGraph";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <di>
+        <div>
           <h1>World wide corona report</h1>
-        </di>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Card>
+              <span>Total Confirmed</span>
+              <br />
+              <span>0</span>
+            </Card>
+            <Card>
+              <span>Total Recovered</span>
+              <br />
+              <span>0</span>
+            </Card>
+            <Card>
+              <span>Total Deaths</span>
+              <br />
+              <span>0</span>
+            </Card>
+          </div>
+        </div>
       </div>
       <LineGraph />
     </div>
